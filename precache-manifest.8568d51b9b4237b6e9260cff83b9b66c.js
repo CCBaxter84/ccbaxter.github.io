9db30c3f15614613bff71b578c100eae7c8da41f
CCBaxter84 <troyceclark@gmail.com>
2020-09-09 12:05:30 -0600

    Updates

---
+++ precache-manifest.8568d51b9b4237b6e9260cff83b9b66c.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "20424e4d93ce2ac871ac14eb0a83421d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ba9616db6da8fef5706f",
+    "url": "/portfolio/static/css/main.de12f44a.chunk.css"
+  },
+  {
+    "revision": "2335c57a74297b0b08e5",
+    "url": "/portfolio/static/js/2.79e9ed1d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/portfolio/static/js/2.79e9ed1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba9616db6da8fef5706f",
+    "url": "/portfolio/static/js/main.d6d01833.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "8b37b3d3ec1c71956fbb5411f45fac71",
+    "url": "/portfolio/static/media/SWJeopardy.8b37b3d3.gif"
+  },
+  {
+    "revision": "a2cff5e3792240601cfe6ef456f6a81c",
+    "url": "/portfolio/static/media/glenwood2.a2cff5e3.jpg"
+  },
+  {
+    "revision": "9ca3fd7c992e9ce4bd39b19b2b00fd78",
+    "url": "/portfolio/static/media/history-quiz.9ca3fd7c.gif"
+  },
+  {
+    "revision": "fc39f5c4065f3306e9149d44f833cba6",
+    "url": "/portfolio/static/media/mern-shopping-list.fc39f5c4.gif"
+  },
+  {
+    "revision": "a13ac5c0bf5d540e4999e76d10a680b8",
+    "url": "/portfolio/static/media/puppies.a13ac5c0.gif"
+  },
+  {
+    "revision": "b62414e8cd0aa37dddb55a0ebd9115e9",
+    "url": "/portfolio/static/media/rps.b62414e8.gif"
+  },
+  {
+    "revision": "ef601eeb7a6d6ff20351b5eb094614d8",
+    "url": "/portfolio/static/media/to-do-app.ef601eeb.gif"
+  }
+]);
