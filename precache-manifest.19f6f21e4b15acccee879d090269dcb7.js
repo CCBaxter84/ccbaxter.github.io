86dfe4d1db925792062c257551e23e52300071c8
CCBaxter84 <troyceclark@gmail.com>
2021-05-19 11:28:05 -0600

    Updates

---
+++ precache-manifest.19f6f21e4b15acccee879d090269dcb7.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81abe271cd12544f09671c107c7ecb87",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6d70c26d56e230800351",
+    "url": "/portfolio/static/css/main.e49e1b62.chunk.css"
+  },
+  {
+    "revision": "ed51843fe9b67c893919",
+    "url": "/portfolio/static/js/2.1dd7e48b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/portfolio/static/js/2.1dd7e48b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6d70c26d56e230800351",
+    "url": "/portfolio/static/js/main.d7f0d986.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "adec840d3cf34839b618eee7b8ee90fe",
+    "url": "/portfolio/static/media/MW.adec840d.gif"
+  },
+  {
+    "revision": "8b37b3d3ec1c71956fbb5411f45fac71",
+    "url": "/portfolio/static/media/SWJeopardy.8b37b3d3.gif"
+  },
+  {
+    "revision": "bfaf26204bad6c0ae54372c8bb0cb1fa",
+    "url": "/portfolio/static/media/glenwood4.bfaf2620.jpg"
+  },
+  {
+    "revision": "9ca3fd7c992e9ce4bd39b19b2b00fd78",
+    "url": "/portfolio/static/media/history-quiz.9ca3fd7c.gif"
+  },
+  {
+    "revision": "fc39f5c4065f3306e9149d44f833cba6",
+    "url": "/portfolio/static/media/mern-shopping-list.fc39f5c4.gif"
+  },
+  {
+    "revision": "a13ac5c0bf5d540e4999e76d10a680b8",
+    "url": "/portfolio/static/media/puppies.a13ac5c0.gif"
+  },
+  {
+    "revision": "bcd09c2c82a063a7d9c34031413a75c8",
+    "url": "/portfolio/static/media/red-october.bcd09c2c.gif"
+  },
+  {
+    "revision": "b62414e8cd0aa37dddb55a0ebd9115e9",
+    "url": "/portfolio/static/media/rps.b62414e8.gif"
+  },
+  {
+    "revision": "9187372d249dfdc45c9622d593f2d5fc",
+    "url": "/portfolio/static/media/star-wars-intro.9187372d.gif"
+  },
+  {
+    "revision": "7a81e5d4d97c95fee1aa9f04c923757f",
+    "url": "/portfolio/static/media/sw-lib.7a81e5d4.gif"
+  },
+  {
+    "revision": "ef601eeb7a6d6ff20351b5eb094614d8",
+    "url": "/portfolio/static/media/to-do-app.ef601eeb.gif"
+  },
+  {
+    "revision": "34fc52a5b29d4ed248a38946fa0d2660",
+    "url": "/portfolio/static/media/weather.34fc52a5.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.241a34d4488cadb35c0e7337224e0ad1.js"
+  "/portfolio/precache-manifest.19f6f21e4b15acccee879d090269dcb7.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
